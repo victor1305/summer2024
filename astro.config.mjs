c72bbe6d194fcf2115682cf0f65af2da4ce25760
Victor <victor.sanchez@phonr.com>
2024-07-31 09:54:05 +0200

    feat: config astro with netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
+import netlifyIntegration from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: netlifyIntegration(),
   integrations: [tailwind(), react()]
 });
