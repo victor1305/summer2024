5f3cbf117d4ada1c9877e0e3203218f0c65ab1c9
Victor <victor.sanchez@phonr.com>
2024-07-31 08:09:33 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), react()]
 });
