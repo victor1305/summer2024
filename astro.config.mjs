e6a9e650aafdba2596742ac65c22de0d8794753e
Victor <victor.sanchez@phonr.com>
2024-07-31 19:47:49 +0200

    feat: config to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
+import react from "@astrojs/react";
+
 export default defineConfig({
   site: "https://summer2024-three.vercel.app",
   output: 'server',
-  adapter: cloudflare(),
+  adapter: vercel(),
   integrations: [tailwind(), react()]
 });
