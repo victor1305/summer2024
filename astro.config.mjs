407b1a878cae53684ecc47d3c35fb247f5e7f63f
Victor <victor.sanchez@phonr.com>
2024-07-31 09:25:24 +0200

    feat: minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercelServerless(),
   integrations: [tailwind(), react()]
 });
